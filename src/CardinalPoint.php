eacb69f67fac5e704d4b891a2a25b0973c8b686a
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2022-07-23 21:54:38 +0200

    refactor: remove a switch that checks cardinal points when executing forward command

--- src/CardinalPoint.php
+++ src/CardinalPoint.php
@@ -5,10 +5,12 @@ namespace GPascual\MarsRover;
 class CardinalPoint
 {
     private string $name;
+    private ?Coordinates $normalVector;
 
-    public function __construct(string $name)
+    public function __construct(string $name, Coordinates $normalVector = null)
     {
         $this->name = $name;
+        $this->normalVector = $normalVector;
     }
 
     public function __toString(): string
@@ -16,27 +18,31 @@ class CardinalPoint
         return $this->name;
     }
 
+    public function getNormalVector(): Coordinates {
+        return $this->normalVector;
+    }
+
     public static function north(): CardinalPoint
     {
         static $north;
-        return $north ?? $north = new CardinalPoint('N');
+        return $north ?? $north = new CardinalPoint('N', new Coordinates(0, 1));
     }
 
     public static function east(): CardinalPoint
     {
         static $east;
-        return $east ?? $east = new CardinalPoint('E');
+        return $east ?? $east = new CardinalPoint('E', new Coordinates(1, 0));
     }
 
     public static function south(): CardinalPoint
     {
         static $south;
-        return $south ?? $south = new CardinalPoint('S');
+        return $south ?? $south = new CardinalPoint('S', new Coordinates(0, -1));
     }
 
     public static function west()
     {
         static $west;
-        return $west ?? $west = new CardinalPoint('W');
+        return $west ?? $west = new CardinalPoint('W', new Coordinates(-1, 0));
     }
 }
--- src/Commands/MoveForward.php
+++ src/Commands/MoveForward.php
@@ -2,26 +2,12 @@
 
 namespace GPascual\MarsRover\Commands;
 
-use GPascual\MarsRover\CardinalPoint;
-use GPascual\MarsRover\Coordinates;
-
 class MoveForward extends Command
 {
     protected function execute(): void
     {
-        switch ($this->rover->orientation()) {
-            case CardinalPoint::north():
-                $this->rover->setPosition($this->rover->position()->add(new Coordinates(0, 1)));
-                break;
-            case CardinalPoint::east():
-                $this->rover->setPosition($this->rover->position()->add(new Coordinates(1, 0)));
-                break;
-            case CardinalPoint::south():
-                $this->rover->setPosition($this->rover->position()->add(new Coordinates(0, -1)));
-                break;
-            case CardinalPoint::west():
-                $this->rover->setPosition($this->rover->position()->add(new Coordinates(-1, 0)));
-                break;
-        }
+        $this->rover->setPosition(
+            $this->rover->position()->add($this->rover->orientation()->getNormalVector())
+        );
     }
 }
